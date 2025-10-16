8121f9cc071a4f6ff8375f692119b82384827478
danyarabdullah111-sys <danyarabdullah111@gmail.com>
2025-10-16 03:47:16 -0700

    Add files via upload

---
+++ person/Person.java
@@ -0,0 +1,19 @@
+
+package person;
+
+
+public class Person {
+
+    
+    public static void main(String[] args) {
+       nac obj=new nac();
+       obj.setName("sarchll");
+       nac obj1=new nac(); 
+       obj.setAge(25);
+        nac obj2=new nac();
+        obj.setCountry("hawler");
+        
+        System.out.println(obj.getAge());    
+    }
+    
+}
---
+++ person/nac.java
@@ -0,0 +1,37 @@
+
+package person;
+
+
+public class nac {
+    private String name;
+    private String country;
+    private  int age;
+    public void setName(String nam){
+    name=nam;
+    }
+    public String getName(){
+   return name; 
+    }
+    
+    public void setAge(int ag){
+    age=ag;
+    }
+     public int getAge(){
+   return age; 
+    }
+    
+     
+    public void setCountry(String countr){
+    country=countr;
+    }
+     public String getCountry(){
+   return country; 
+    }
+    
+    
+    
+    
+    
+    
+    
+}
